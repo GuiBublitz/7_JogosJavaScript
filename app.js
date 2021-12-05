cb1dc6cfc517be7a8e04273b4f45a7ce6e4950cc
Guilherme Bublitz <gui.e.bublitz@gmail.com>
2021-12-05 10:13:57 -0200

    MemoryGame done

--- app.js
+++ app.js
@@ -52,14 +52,55 @@ document.addEventListener('DOMContentLoaded', ()=>{
         }
     ]
 
+    cardArray.sort(() => 0.5 - Math.random())
+
     const grid = document.querySelector('.grid');
+    const resultDisplay = document.querySelector('#result')
+    var cardsChosen = [];
+    var cardsChosenId = [];
+    var cardsWon = []
 
     //create your board
     function createBoard(){
         for(let i = 0; i < cardArray.length; i++){
             var card = document.createElement('img')
             card.setAttribute('src', 'img/icons8-yellow-square-96.png')
+            card.setAttribute('data-id', i)
+            card.addEventListener('click',flipcard)
+            grid.appendChild(card)
         }
     }
+    function checkForMatch(){
+        var cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+        if(cardsChosen[0] === cardsChosen[1]){
+            alert('You found a match')
+            cards[optionOneId].setAttribute('src', 'img/icons8-white-large-square-96.png')  
+            cards[optionTwoId].setAttribute('src', 'img/icons8-white-large-square-96.png')  
+            cardsWon.push(cardsChosen)
+        } else{
+            cards[optionOneId].setAttribute('src', 'img/icons8-yellow-square-96.png')  
+            cards[optionTwoId].setAttribute('src', 'img/icons8-yellow-square-96.png') 
+            alert('Sorry, try again')
+        }
+        cardsChosen = []
+        cardsChosenId = []
+        resultDisplay.textContent = cardsWon.length
+        if(cardsWon.length === cardArray.length/2){
+            resultDisplay.textContent = 'Congratulations! You found them all!'
+        }
+    }
+    function flipcard(){
+        var cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if (cardsChosen.length === 2){
+            setTimeout(checkForMatch, 500)
+        }
+    }
+
+
     createBoard();
 })
